89ab5787aad36a87085f1918cda083532bc56075
Favour Egwele <favoregwele@gmail.com>
2025-05-05 09:53:55 -1200

    Done with landing for now

--- landing.js
+++ landing.js
@@ -1,3 +1,24 @@
+const cart = document.querySelectorAll('.cart-track');
+const cartCount = document.querySelector('.cart-count p');
+let cartCounting = 0;
+
+cart.forEach((cartBtn) => {
+    cartBtn.addEventListener('click', () => {
+        if(cartBtn.classList.contains('bi-cart2')){
+            cartBtn.classList.remove('bi-cart2');
+            cartBtn.classList.add('bi-cart-check');
+            cartCounting++;
+            cartCount.innerHTML = cartCounting;    
+        } else if(cartBtn.classList.contains('bi-cart-check')) {
+            cartBtn.classList.remove('bi-cart-check');
+            cartBtn.classList.add('bi-cart2');
+            cartCounting--;
+            cartCount.innerHTML = cartCounting;
+        }
+
+        console.log('clicked');
+    })
+})
 
 // //NAVIGATION TOGGLE
 // function onOff(button) {
