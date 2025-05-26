92cee1166430eabac2ee45b16ff88b6f7afd1683
Favour Egwele <favoregwele@gmail.com>
2025-05-25 21:27:39 -1200

    overall total calculation

--- new-js-files/checkoutO.js
+++ new-js-files/checkoutO.js
@@ -1,3 +1,4 @@
+import { products } from "./General files/productsO.js";
 import { getCartCount, getCartStorage } from "./General files/utilsO.js";
 
 const cartCount = getCartCount();
@@ -8,7 +9,6 @@ pageCartCount.textContent = `${cartCount} item${cartCount === 1 ? '' : 's'}`;
 
 const cartStorage = getCartStorage();
 const itemsContainer = document.querySelector('.items-container');
-const theTotal = document.querySelector('.checkout-products');
 
     cartStorage.forEach(item => {
         itemsContainer.innerHTML += `<div class="cart-item">
@@ -19,7 +19,7 @@ const theTotal = document.querySelector('.checkout-products');
                 <img src="${item.image}" alt="">
                 <div class="product-info">
                     <h3>${item.name}</h3>
-                    <p>Status: Damaged</p>
+                    <p>Status: Perfect</p>
                     <p>Size: Medium</p>
                     <p>Location: Austen, Texas</p>
                 </div>
@@ -28,18 +28,42 @@ const theTotal = document.querySelector('.checkout-products');
             <div class="others">
                 <p class="price">$${(item.priceCents / 100).toFixed(2)}</p>
                 <p class="delivery">$5.40</p>
-                <p class="total">$25.90</p>
+                <p class="total" data-product-id="${item.id}">$25.90</p>
             </div>
 
         </div>`;
-    
-        const delivery = document.querySelector('.delivery').textContent;
-        const deliveryPrice = Number(delivery.replace('$', '')) * 100;
-        const priceProduct = document.querySelector('.total');
-        priceProduct.innerHTML = `$${((deliveryPrice + item.priceCents) / 100).toFixed(2)}`;
 
-        theTotal.innerHTML += `<div class="checkout-product">
-            <p>${item.name}</p>
-            <p>${priceProduct.textContent}</p>
-        </div>`;
-});
+    });
+
+
+    const delivery = document.querySelector('.delivery').textContent;
+    const deliveryPrice = Number(delivery.replace('$', '')) * 100;
+    const productTotal = document.querySelectorAll('.total');
+    const theTotal = document.querySelector('.checkout-products');
+    let testThree = 0;
+    
+    productTotal.forEach(total => {
+
+        const productTotalId = total.dataset.productId;
+    
+        products.forEach(product => {
+
+            if (productTotalId === product.id){
+   
+                total.innerHTML = `$${((deliveryPrice + product.priceCents) / 100).toFixed(2)}`;
+            
+                theTotal.innerHTML += `<div class="checkout-product">
+                <p>${product.name}</p>
+                <p>${total.textContent}</p>
+                </div>`;
+
+            }
+
+        });
+
+        const test = total.textContent;
+        const testTwo = Number(test.replace('$', '')) * 100;
+        testThree += testTwo;
+        document.querySelector('.overall-total').textContent = `$${(testThree / 100).toFixed(2)}`;
+
+    });
