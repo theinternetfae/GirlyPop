b3a1c2cf2d070b5529b6e562947d2911405ea5d0
Favour Egwele <favoregwele@gmail.com>
2026-04-10 15:09:44 +0200

    Setting up error display toggle

--- new-js-files/landingO.js
+++ new-js-files/landingO.js
@@ -4,9 +4,22 @@ import { updateCartCount, featuredProducts } from "./General files/utilsO.js";
 // updateCartCount();
 
 const soon = document.querySelectorAll(".soon");
+const alert = document.querySelector(".alert-cont");
+const exit = document.querySelector(".exit-alert");
 
 soon.forEach(s => {
-    s.addEventListener("click", () => {
-        console.log("Clicked!")
+    s.addEventListener("click", (e) => {
+        e.preventDefault();
+        if(alert.classList.contains('out')) {
+            return;
+        } else {
+            alert.classList.add('out');
+        }
     })
 })
+
+exit.addEventListener("click", () => {
+    console.log("Exit clicked");
+    alert.classList.remove('out');
+})
+
