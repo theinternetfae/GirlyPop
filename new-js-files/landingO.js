660057bc2d6e9f41483314dd5f7c507d7443a8b0
Favour Egwele <favoregwele@gmail.com>
2026-04-10 14:23:08 +0200

    Set up alert

--- new-js-files/landingO.js
+++ new-js-files/landingO.js
@@ -2,3 +2,11 @@ import { updateCartCount, featuredProducts } from "./General files/utilsO.js";
 
 // featuredProducts();
 // updateCartCount();
+
+const soon = document.querySelectorAll(".soon");
+
+soon.forEach(s => {
+    s.addEventListener("click", () => {
+        console.log("Clicked!")
+    })
+})
