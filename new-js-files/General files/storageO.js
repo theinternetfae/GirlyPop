e6bc56e732219157da6fafa949543426bab1bf7f
Favour Egwele <favoregwele@gmail.com>
2026-05-16 17:30:59 +0200

    Fixed alert on main pages

--- new-js-files/General files/storageO.js
+++ new-js-files/General files/storageO.js
@@ -7,29 +7,3 @@ export function getCart() {
 export function setCart(newCart) {
     localStorage.setItem('inCart', JSON.stringify(newCart));
 }
-
-// export function cartCountStorage() {
-//     let cartCount = parseInt(localStorage.getItem('cartCount')) || 0;
-
-//     localStorage.setItem('cartCount', cartCount);
-
-//     return cartCount;
-// }
-
-// export function cartStorageDisplay() {
-//     let cartStorage = JSON.parse(localStorage.getItem('cartStorage')) || [];
-
-//     localStorage.setItem('cartStorage', JSON.stringify(cartStorage));
-
-//     return cartStorage;
-// }
-
-// export function saveCartButtonState(productIds) {
-//     localStorage.setItem('addedToCartIds', JSON.stringify(productIds));
-// }
-
-// export function getSavedCartButtonState() {
-//     return JSON.parse(localStorage.getItem('addedToCartIds')) || [];
-// }
-
-// // localStorage.clear();
--- new-js-files/General files/utilsO.js
+++ new-js-files/General files/utilsO.js
@@ -91,8 +91,48 @@ export function initializeNavBar() {
         c.innerHTML = inCart.length;
     });
 
+
+
+
+    //ALERT 
+    const soon = document.querySelectorAll(".soon");
+    const alert = document.querySelector(".alert-cont");
+    const exit = document.querySelector(".exit-alert");
+
+    soon.forEach(s => {
+        s.addEventListener("click", (e) => {
+            e.preventDefault();
+            if(alert.classList.contains('out')) {
+                return;
+            } else {
+                if(navsSm.classList.contains('show')){
+                    navsSm.classList.remove('show');
+                }
+                alert.classList.add('out');
+                automaticClear(alert);
+            }
+        })
+    })
+
+    exit.addEventListener("click", () => {
+        console.log("Exit clicked");
+        alert.classList.remove('out');
+    })
+
 }
 
+
+function automaticClear(alert) {
+
+    if(alert.classList.contains('out')) {
+        const timer = setTimeout(() => {
+            alert.classList.remove('out');
+        }, 8000)
+    
+        return () => clearTimeout(timer);
+    }
+
+}
 
 
 
